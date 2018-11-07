e14e30e0f209956151a6af2a13a22b21bd2b6961
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-11-07 16:44:06 +1100

    Revert "removed support for 1.12.x, we now only support 1.12.2"
    This reverts commit 239db8780936f080b7ef852d17fbfde39fbc5df6.

--- src/main/java/cadiboo/renderchunkrebuildchunkhooks/loadingplugin/RenderChunkRebuildChunkHooksLoadingPlugin1_12_1.java
+++ src/main/java/cadiboo/renderchunkrebuildchunkhooks/loadingplugin/RenderChunkRebuildChunkHooksLoadingPlugin1_12_1.java
@@ -3,7 +3,7 @@ package cadiboo.renderchunkrebuildchunkhooks.loadingplugin;
 //import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.*;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 
-@MCVersion("1.12.2")
-public class RenderChunkRebuildChunkHooksLoadingPlugin1_12_2 extends RenderChunkRebuildChunkHooksLoadingPlugin1_12_1 {
+@MCVersion("1.12.1")
+public class RenderChunkRebuildChunkHooksLoadingPlugin1_12_1 extends RenderChunkRebuildChunkHooksLoadingPlugin1_12 {
 
 }
