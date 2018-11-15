838b5df94b69ef98dafd6a3e521072dbfb4e7a02
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-11-15 21:22:27 +1100

    Clean start for new hooks

--- src/main/java/cadiboo/renderchunkrebuildchunkhooks/core/RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine.java
+++ src/main/java/cadiboo/renderchunkrebuildchunkhooks/core/RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine.java
@@ -1,51 +1,6 @@
 package cadiboo.renderchunkrebuildchunkhooks.core;
 
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.lang.reflect.Field;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.ListIterator;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Label;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldInsnNode;
-import org.objectweb.asm.tree.FieldNode;
-import org.objectweb.asm.tree.FrameNode;
 import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.IntInsnNode;
-import org.objectweb.asm.tree.JumpInsnNode;
-import org.objectweb.asm.tree.LabelNode;
-import org.objectweb.asm.tree.LineNumberNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
-import org.objectweb.asm.tree.TypeInsnNode;
-import org.objectweb.asm.tree.VarInsnNode;
-import org.objectweb.asm.util.Printer;
-import org.objectweb.asm.util.Textifier;
-import org.objectweb.asm.util.TraceClassVisitor;
-import org.objectweb.asm.util.TraceMethodVisitor;
-
-import com.google.common.collect.ImmutableList;
-
-import cadiboo.renderchunkrebuildchunkhooks.core.util.INames;
-import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockEvent;
-import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPreEvent;
-import cadiboo.renderchunkrebuildchunkhooks.hooks.RenderChunkRebuildChunkHooksHooks;
-import net.minecraft.client.renderer.chunk.ChunkCompileTaskGenerator;
-import net.minecraft.client.renderer.chunk.RenderChunk;
-import net.minecraft.launchwrapper.IClassTransformer;
-import net.minecraft.util.BlockRenderLayer;
 
 /**
  * @author Cadiboo
@@ -54,186 +9,49 @@ import net.minecraft.util.BlockRenderLayer;
 // useful links:
 // https://text-compare.com
 // http://www.minecraftforge.net/forum/topic/32600-1710-strange-error-with-custom-event-amp-event-handler/?do=findComment&comment=172480
-public class RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine implements IClassTransformer, Opcodes, INames {
+public class RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine extends RenderChunkRebuildChunkHooksRenderChunkClassTransformer {
 
-	public static final List<String> IGNORED_PREFIXES = ImmutableList.of("cpw", "net.minecraftforge", "io", "org", "gnu", "com", "joptsimple");
+    @Override
+    public byte[] transform(final String unTransformedName, final String transformedName, final byte[] basicClass) {
 
-	public static final int CLASS_WRITER_FLAGS = ClassWriter.COMPUTE_MAXS | ClassWriter.COMPUTE_FRAMES;
-	// skip class reader rJeading frames if the class writer is going to compute them for us (if it is you should get a warning that this being 0 is dead code)
-	public static final int CLASS_READER_FLAGS = (CLASS_WRITER_FLAGS & ClassWriter.COMPUTE_FRAMES) == ClassWriter.COMPUTE_FRAMES ? ClassReader.SKIP_FRAMES : 0;
-
-	public static final Logger LOGGER = LogManager.getLogger();
-
-	private static final boolean DEBUG_DUMP_BYTECODE = true;
-
-	public static final boolean	DEBUG_EVERYTHING	= false;
-	public static final boolean	DEBUG_CLASSES		= DEBUG_EVERYTHING | false;
-	public static final boolean	DEBUG_TYPES			= DEBUG_EVERYTHING | true;
-	public static final boolean	DEBUG_METHODS		= DEBUG_EVERYTHING | true;
-	public static final boolean	DEBUG_INSTRUCTIONS	= DEBUG_EVERYTHING | true;
-	public static final boolean	DEBUG_FIELDS		= DEBUG_EVERYTHING | true;
-
-	static {
-		if (DEBUG_TYPES) {
-			for (final Field field : INames.class.getFields()) {
-				Object value;
-				try {
-					value = field.get(INames.class);
-
-					LOGGER.info(field.getName() + ": " + value);
-
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					LOGGER.error("Error in <clinit>!");
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	@Override
-	public byte[] transform(final String unTransformedName, final String transformedName, final byte[] basicClass) {
-
-		if (DEBUG_CLASSES) {
-			if ((unTransformedName.startsWith("b") || unTransformedName.startsWith("net.minecraft.client.renderer.chunk.")) || (transformedName.startsWith("b") || transformedName.startsWith("net.minecraft.client.renderer.chunk."))) {
-				LOGGER.info("unTransformedName: " + unTransformedName + ", transformedName: " + transformedName + ", unTransformedName equals: " + unTransformedName.equals(RENDER_CHUNK_TRANSFORMED_NAME) + ", transformedName equals: " + transformedName.equals(RENDER_CHUNK_TRANSFORMED_NAME));
-			}
-		}
-
-		if (!transformedName.equals(RENDER_CHUNK_TRANSFORMED_NAME)) {
-			return basicClass;
-		}
-
-		if (DEBUG_DUMP_BYTECODE) {
-			try {
-				final Path pathToFile = Paths.get("/Users/" + System.getProperty("user.name") + "/Desktop/before_hooks" + /* (System.nanoTime() & 0xFF) + */ ".txt");
-				final PrintWriter filePrinter = new PrintWriter(pathToFile.toFile());
-				final ClassReader reader = new ClassReader(basicClass);
-				final TraceClassVisitor tracingVisitor = new TraceClassVisitor(filePrinter);
-				reader.accept(tracingVisitor, 0);
-
-				final Path pathToClass = Paths.get("/Users/" + System.getProperty("user.name") + "/Desktop/before_hooks" + /* (System.nanoTime() & 0xFF) + */ ".class");
-				final FileOutputStream fileOutputStream = new FileOutputStream(pathToClass.toFile());
-				fileOutputStream.write(basicClass);
-				fileOutputStream.close();
-			} catch (final Exception e) {
-				// TODO: handle exception
-			}
-		}
-
-		LOGGER.info("Preparing to inject hooks into \"" + transformedName + "\" (RenderChunk)");
-
-		// Build classNode & get instruction list
-		final ClassNode classNode = new ClassNode();
-		final ClassReader cr = new ClassReader(basicClass);
-		cr.accept(classNode, CLASS_READER_FLAGS);
-
-		if (DEBUG_TYPES) {
-			LOGGER.info("RebuildChunk type: " + REBUILD_CHUNK_TYPE);
-			LOGGER.info("RebuildChunk descriptor: " + REBUILD_CHUNK_DESCRIPTOR);
-		}
-
-		// peek at classNode and modifier
-		for (final MethodNode method : classNode.methods) {
-
-			if (!method.desc.equals(REBUILD_CHUNK_DESCRIPTOR)) {
-				if (DEBUG_METHODS) {
-					LOGGER.info("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" did not match");
-				}
-				continue;
-			}
-
-			if (DEBUG_METHODS) {
-				LOGGER.info("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" matched!");
-			}
-
-			// make sure not to overwrite resortTransparency (it has the same description but it's name is "a" while rebuildChunk's name is "b")
-			if (method.name.equals("a") || method.name.equals("resortTransparency")) {
-				if (DEBUG_METHODS) {
-					LOGGER.info("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" was rejected");
-				}
-				continue;
-			}
-
-			if (DEBUG_METHODS) {
-				LOGGER.info("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" matched and passed");
-			}
-
-			// inject instructions
-			this.injectRebuildChunkPreEventHook(method.instructions);
-			this.injectRebuildChunkBlockRenderInLayerEventHook(method.instructions);
-			this.injectRebuildRebuildChunkBlockEventHook(method.instructions);
-
-		}
-
-		// write classNode
-		try {
-			final ClassWriter out = new ClassWriter(CLASS_WRITER_FLAGS);
-
-			// make the ClassWriter visit all the code in classNode
-			classNode.accept(out);
-
-			LOGGER.info("Injected hooks sucessfully!");
-
-			if (DEBUG_DUMP_BYTECODE) {
-				try {
-					final byte[] bytes = out.toByteArray();
-
-					final Path pathToFile = Paths.get("/Users/" + System.getProperty("user.name") + "/Desktop/after_hooks" + /* (System.nanoTime() & 0xFF) + */ ".txt");
-					final PrintWriter filePrinter = new PrintWriter(pathToFile.toFile());
-					final ClassReader reader = new ClassReader(bytes);
-					final TraceClassVisitor tracingVisitor = new TraceClassVisitor(filePrinter);
-					reader.accept(tracingVisitor, 0);
-
-					final Path pathToClass = Paths.get("/Users/" + System.getProperty("user.name") + "/Desktop/after_hooks" + /* (System.nanoTime() & 0xFF) + */ ".class");
-					final FileOutputStream fileOutputStream = new FileOutputStream(pathToClass.toFile());
-					fileOutputStream.write(bytes);
-					fileOutputStream.close();
-				} catch (final Exception e) {
-					// TODO: handle exception
-				}
-			}
-
-			return out.toByteArray();
-		} catch (final Exception e) {
-			e.printStackTrace();
-			LOGGER.error("FAILED to inject hooks!!! Discarding changes.");
-			LOGGER.warn("Any mods that depend on the hooks provided by this mod will break");
-			return basicClass;
-		}
-
-	}
-
-    private void injectRebuildChunkPreEventHook(InsnList instructions) {
-
-	}
-
-	private void injectRebuildChunkBlockRenderInLayerEventHook(InsnList instructions) {
+        return super.transform(unTransformedName, transformedName, basicClass);
 
     }
 
-    private void injectRebuildRebuildChunkBlockEventHook(InsnList instructions) {
+    public void injectHooks(InsnList instructions) {
+
+        super.injectHooks(instructions);
 
     }
 
-    private static final Printer PRINTER = new Textifier();
+    public void injectRebuildChunkPreEventHook(InsnList instructions) {
 
-	public static String insnToString(final AbstractInsnNode insn) {
-		insn.accept(TRACE_METHOD_VISITOR);
-		final StringWriter sw = new StringWriter();
-		PRINTER.print(new PrintWriter(sw));
-		PRINTER.getText().clear();
-		return sw.toString().trim();
-	}
+        // get "++renderChunksUpdated;"
+        // inject after
+        // get line number for nice debug
 
-	private static final TraceMethodVisitor TRACE_METHOD_VISITOR = new TraceMethodVisitor(PRINTER);
+//        GETSTATIC net/minecraft/client/renderer/chunk/RenderChunk.renderChunksUpdated : I
+//                ICONST_1
+//        IADD
+//        PUTSTATIC net/minecraft/client/renderer/chunk/RenderChunk.renderChunksUpdated : I
 
-	public static String fieldToString(final FieldNode field) {
-		final StringWriter sw = new StringWriter();
-		final PrintWriter pw = new PrintWriter(sw);
-		field.accept(new TraceClassVisitor(pw));
-		PRINTER.print(pw);
-		PRINTER.getText().clear();
-		return sw.toString().trim();
-	}
+    }
+
+    public void injectRebuildChunkBlockRenderInLayerEventHook(InsnList instructions) {
+
+        // get "block.canRenderInLayer(iblockstate, blockrenderlayer);"
+        // "INVOKEVIRTUAL net/minecraft/block/Block.canRenderInLayer (Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/util/BlockRenderLayer;)Z"
+        // replace it with our hook and inject before & after (let the existing ALOADs stay because we use the same variables)
+
+    }
+
+    public void injectRebuildRebuildChunkBlockEventHook(InsnList instructions) {
+
+        // find "blockrendererdispatcher.renderBlock(iblockstate, blockpos$mutableblockpos, this.worldView, bufferbuilder);"
+        // "INVOKEVIRTUAL net/minecraft/client/renderer/BlockRendererDispatcher.renderBlock (Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/client/renderer/BufferBuilder;)Z"
+        // get return label
+        // get line number for nice debug
+        // inject before
+    }
 
 }
