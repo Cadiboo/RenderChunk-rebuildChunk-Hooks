c953a6983c9cd2455b1aa7c5a5abe768722e849c
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-11-16 14:52:14 +1100

    added parts for RebuildChunkPostEvent

---
+++ src/main/java/cadiboo/renderchunkrebuildchunkhooks/event/RebuildChunkPostEvent.java
@@ -0,0 +1,127 @@
+package cadiboo.renderchunkrebuildchunkhooks.event;
+
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.chunk.ChunkCompileTaskGenerator;
+import net.minecraft.client.renderer.chunk.CompiledChunk;
+import net.minecraft.client.renderer.chunk.RenderChunk;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
+import net.minecraft.world.ChunkCache;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+/**
+ * Called when a {@link RenderChunk#rebuildChunk RenderChunk.rebuildChunk} is called.<br>
+ * This event is fired on the {@link net.minecraftforge.common.MinecraftForge#EVENT_BUS EVENT_BUS} right before RenderChunk#rebuildChunk returns.<br>
+ *
+ * @author Cadiboo
+ * @see RenderChunk#rebuildChunk(float, float, float, ChunkCompileTaskGenerator)
+ */
+//TODO make this
+public class RebuildChunkPostEvent extends Event {
+
+	private final RenderChunk               renderChunk;
+	private final RenderGlobal              context;
+	private final ChunkCache                worldView;
+	private final ChunkCompileTaskGenerator generator;
+	private final CompiledChunk             compiledChunk;
+	private final MutableBlockPos           renderChunkPosition;
+	private final float                     x;
+	private final float                     y;
+	private final float                     z;
+
+	/**
+	 * @param renderChunk         the instance of {@link RenderChunk} the event is being fired for
+	 * @param x                   the translation X passed in from RenderChunk#rebuildChunk
+	 * @param y                   the translation Y passed in from RenderChunk#rebuildChunk
+	 * @param z                   the translation Z passed in from RenderChunk#rebuildChunk
+	 * @param renderChunkPosition the {@link MutableBlockPos position} passed in from RenderChunk#rebuildChunk
+	 * @param worldView           the {@link ChunkCache} passed in from RenderChunk#rebuildChunk
+	 * @param renderGlobal        the {@link RenderGlobal} passed in from RenderChunk#rebuildChunk
+	 * @param generator           the {@link ChunkCompileTaskGenerator} passed in from RenderChunk#rebuildChunk
+	 * @param compiledChunk       the {@link CompiledChunk} passed in from RenderChunk#rebuildChunk
+	 */
+	public RebuildChunkPostEvent(final RenderChunk renderChunk, final RenderGlobal renderGlobal, final ChunkCache worldView, final ChunkCompileTaskGenerator generator, final CompiledChunk compiledChunk, final MutableBlockPos renderChunkPosition, final float x, final float y, final float z) {
+
+		this.renderChunk = renderChunk;
+		this.context = renderGlobal;
+		this.worldView = worldView;
+		this.generator = generator;
+		this.compiledChunk = compiledChunk;
+		this.renderChunkPosition = renderChunkPosition;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	/**
+	 * @return the instance of {@link RenderChunk} the event is being fired for
+	 */
+	public RenderChunk getRenderChunk() {
+
+		return this.renderChunk;
+	}
+
+	/**
+	 * @return the {@link RenderGlobal} passed in
+	 */
+	public RenderGlobal getContext() {
+
+		return this.context;
+	}
+
+	/**
+	 * @return the {@link ChunkCache} passed in
+	 */
+	public ChunkCache getWorldView() {
+
+		return this.worldView;
+	}
+
+	/**
+	 * @return the {@link ChunkCompileTaskGenerator} passed in
+	 */
+	public ChunkCompileTaskGenerator getGenerator() {
+
+		return this.generator;
+	}
+
+	/**
+	 * @return the {@link CompiledChunk} passed in
+	 */
+	public CompiledChunk getCompiledChunk() {
+
+		return this.compiledChunk;
+	}
+
+	/**
+	 * @return the position passed in
+	 */
+	public MutableBlockPos getRenderChunkPosition() {
+
+		return this.renderChunkPosition;
+	}
+
+	/**
+	 * @return the X passed in
+	 */
+	public float getX() {
+
+		return this.x;
+	}
+
+	/**
+	 * @return the Y passed in
+	 */
+	public float getY() {
+
+		return this.y;
+	}
+
+	/**
+	 * @return the Z passed in
+	 */
+	public float getZ() {
+
+		return this.z;
+	}
+
+}
