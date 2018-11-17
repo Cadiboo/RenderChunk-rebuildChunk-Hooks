cb5a1e1cf679e2ed273cbd99ec7e428748a0bf12
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-11-17 11:24:17 +1100

    disable half-done optifine compat for build

--- src/main/java/cadiboo/renderchunkrebuildchunkhooks/core/RenderChunkRebuildChunkHooksLoadingPlugin1_12_2.java
+++ src/main/java/cadiboo/renderchunkrebuildchunkhooks/core/RenderChunkRebuildChunkHooksLoadingPlugin1_12_2.java
@@ -45,9 +45,9 @@ public class RenderChunkRebuildChunkHooksLoadingPlugin1_12_2 implements IFMLLoad
 	@Override
 	public String[] getASMTransformerClass() {
 		//		if (OPTIFINE) {
-		return new String[] { RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine.class.getName() };
+//		return new String[] { RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine.class.getName() };
 		//		}
-		//		return new String[] { RenderChunkRebuildChunkHooksRenderChunkClassTransformer.class.getName() };
+				return new String[] { RenderChunkRebuildChunkHooksRenderChunkClassTransformer.class.getName() };
 		//		return new String[0];
 	}
 
--- src/main/java/cadiboo/renderchunkrebuildchunkhooks/core/RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine.java
+++ src/main/java/cadiboo/renderchunkrebuildchunkhooks/core/RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine.java
@@ -1,86 +1,86 @@
-package cadiboo.renderchunkrebuildchunkhooks.core;
-
-import org.objectweb.asm.tree.InsnList;
-
-/**
- * @author Cadiboo
- * @see <a href="http://www.egtry.com/java/bytecode/asm/tree_transform">http://www.egtry.com/java/bytecode/asm/tree_transform</a>
- */
-// useful links:
-// https://text-compare.com
-// http://www.minecraftforge.net/forum/topic/32600-1710-strange-error-with-custom-event-amp-event-handler/?do=findComment&comment=172480
-public class RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine extends RenderChunkRebuildChunkHooksRenderChunkClassTransformerVanillaForge {
-
-	@Override
-	public byte[] transform(final String unTransformedName, final String transformedName, final byte[] basicClass) {
-
-		return super.transform(unTransformedName, transformedName, basicClass);
-
-	}
-
-	@Override
-	public void injectHooks(InsnList instructions) {
-
-		super.injectHooks(instructions);
-
-	}
-
-	/**
-	 * get "++renderChunksUpdated;"
-	 * inject after
-	 * get line number for nice debug
-	 *
-	 * @param instructions the instructions for the method
-	 */
-	@Override
-	public void injectRebuildChunkPreEventHook(InsnList instructions) {
-
-		super.injectRebuildChunkPreEventHook(instructions);
-
-	}
-
-	/**
-	 * get "block.canRenderInLayer(iblockstate, blockrenderlayer);"<br>
-	 * "INVOKEVIRTUAL net/minecraft/block/Block.canRenderInLayer (Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/util/BlockRenderLayer;)Z"<br>
-	 * replace it with our hook and inject before & after (let the existing ALOADs stay because we use the same variables)<br>
-	 *
-	 * @param instructions the instructions for the method
-	 */
-	@Override
-	public void injectRebuildChunkBlockRenderInLayerEventHook(InsnList instructions) {
-
-		super.injectRebuildChunkBlockRenderInLayerEventHook(instructions);
-
-	}
-
-	/**
-	 * find "blockrendererdispatcher.renderBlock(iblockstate, blockpos$mutableblockpos, this.worldView, bufferbuilder);"<br>
-	 * "INVOKEVIRTUAL net/minecraft/client/renderer/BlockRendererDispatcher.renderBlock (Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/client/renderer/BufferBuilder;)Z"<br>
-	 * get return label<br>
-	 * get line number for nice debug<br>
-	 * inject before<br>
-	 *
-	 * @param instructions
-	 */
-	@Override
-	public void injectRebuildChunkBlockEventHook(InsnList instructions) {
-
-		super.injectRebuildChunkBlockEventHook(instructions);
-
-	}
-
-	/**
-	 * find last return statement in method<br>
-	 * get line number for nice debug<br>
-	 * inject before<br>
-	 *
-	 * @param instructions
-	 */
-	@Override
-	public void injectRebuildChunkPostEventHook(InsnList instructions) {
-
-		super.injectRebuildChunkPostEventHook(instructions);
-
-	}
-
-}
+//package cadiboo.renderchunkrebuildchunkhooks.core;
+//
+//import org.objectweb.asm.tree.InsnList;
+//
+///**
+// * @author Cadiboo
+// * @see <a href="http://www.egtry.com/java/bytecode/asm/tree_transform">http://www.egtry.com/java/bytecode/asm/tree_transform</a>
+// */
+//// useful links:
+//// https://text-compare.com
+//// http://www.minecraftforge.net/forum/topic/32600-1710-strange-error-with-custom-event-amp-event-handler/?do=findComment&comment=172480
+//public class RenderChunkRebuildChunkHooksRenderChunkClassTransformerOptifine extends RenderChunkRebuildChunkHooksRenderChunkClassTransformerVanillaForge {
+//
+//	@Override
+//	public byte[] transform(final String unTransformedName, final String transformedName, final byte[] basicClass) {
+//
+//		return super.transform(unTransformedName, transformedName, basicClass);
+//
+//	}
+//
+//	@Override
+//	public void injectHooks(InsnList instructions) {
+//
+//		super.injectHooks(instructions);
+//
+//	}
+//
+//	/**
+//	 * get "++renderChunksUpdated;"
+//	 * inject after
+//	 * get line number for nice debug
+//	 *
+//	 * @param instructions the instructions for the method
+//	 */
+//	@Override
+//	public void injectRebuildChunkPreEventHook(InsnList instructions) {
+//
+//		super.injectRebuildChunkPreEventHook(instructions);
+//
+//	}
+//
+//	/**
+//	 * get "block.canRenderInLayer(iblockstate, blockrenderlayer);"<br>
+//	 * "INVOKEVIRTUAL net/minecraft/block/Block.canRenderInLayer (Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/util/BlockRenderLayer;)Z"<br>
+//	 * replace it with our hook and inject before & after (let the existing ALOADs stay because we use the same variables)<br>
+//	 *
+//	 * @param instructions the instructions for the method
+//	 */
+//	@Override
+//	public void injectRebuildChunkBlockRenderInLayerEventHook(InsnList instructions) {
+//
+//		super.injectRebuildChunkBlockRenderInLayerEventHook(instructions);
+//
+//	}
+//
+//	/**
+//	 * find "blockrendererdispatcher.renderBlock(iblockstate, blockpos$mutableblockpos, this.worldView, bufferbuilder);"<br>
+//	 * "INVOKEVIRTUAL net/minecraft/client/renderer/BlockRendererDispatcher.renderBlock (Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/client/renderer/BufferBuilder;)Z"<br>
+//	 * get return label<br>
+//	 * get line number for nice debug<br>
+//	 * inject before<br>
+//	 *
+//	 * @param instructions
+//	 */
+//	@Override
+//	public void injectRebuildChunkBlockEventHook(InsnList instructions) {
+//
+//		super.injectRebuildChunkBlockEventHook(instructions);
+//
+//	}
+//
+//	/**
+//	 * find last return statement in method<br>
+//	 * get line number for nice debug<br>
+//	 * inject before<br>
+//	 *
+//	 * @param instructions
+//	 */
+//	@Override
+//	public void injectRebuildChunkPostEventHook(InsnList instructions) {
+//
+//		super.injectRebuildChunkPostEventHook(instructions);
+//
+//	}
+//
+//}
