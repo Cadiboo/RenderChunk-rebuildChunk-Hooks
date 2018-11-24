cbcd3ea83aa1c07e426bff723c0ad7ee356be640
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-11-24 11:11:12 +1100

    Update BetterFoliageCompatibilityEventSubscriber.java

--- src/main/java/cadiboo/renderchunkrebuildchunkhooks/mod/BetterFoliageCompatibilityEventSubscriber.java
+++ src/main/java/cadiboo/renderchunkrebuildchunkhooks/mod/BetterFoliageCompatibilityEventSubscriber.java
@@ -2,7 +2,8 @@ package cadiboo.renderchunkrebuildchunkhooks.mod;
 
 import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockEvent;
 import cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockRenderInLayerEvent;
-import cadiboo.renderchunkrebuildchunkhooks.event.optifine.RebuildChunkBlockOptifineEvent;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
@@ -22,27 +23,28 @@ public class BetterFoliageCompatibilityEventSubscriber {
 	@SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = false)
 	public void callBetterFoliageHooks_renderWorldBlock_onRebuildChunkBlockEvent(final RebuildChunkBlockEvent event) {
 
-		mods.betterfoliage.client.Hooks.renderWorldBlock(event.getBlockRendererDispatcher(), event.getBlockState(), event.getBlockPos(), event.getChunkCache(), event.getBufferBuilder(), event.getBlockRenderLayer());
+		final BlockRenderLayer blockRenderLayer = event.getBlockRenderLayer();
+
+		boolean used = mods.betterfoliage.client.Hooks.renderWorldBlock(event.getBlockRendererDispatcher(), event.getBlockState(), event.getBlockPos(), event.getChunkCache(), event.getBufferBuilder(), blockRenderLayer);
+
+		event.getUsedBlockRenderLayers()[blockRenderLayer.ordinal()] = used;
 
 		event.setCanceled(true);
-	}
 
-	//because BetterFoliage's name starts with "b" it will get called early anyway, and I want to keep HIGHEST and HIGH for mods that actually need them
-	@SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = false)
-	public void callBetterFoliageHooks_renderWorldBlock_onRebuildChunkBlockEventOptifine(final RebuildChunkBlockOptifineEvent event) {
-
-		mods.betterfoliage.client.Hooks.renderWorldBlock(event.getBlockRendererDispatcher(), event.getBlockState(), event.getBlockPos(), event.getChunkCache(), event.getBufferBuilder(), event.getBlockRenderLayer());
-
-		event.setCanceled(true);
 	}
 
 	//because BetterFoliage's name starts with "b" it will get called early anyway, and I want to keep HIGHEST and HIGH for mods that actually need them
 	@SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = false)
 	public void callBetterFoliageHooks_canRenderBlockInLayer_onRebuildChunkBlockRenderInLayerEvent(final RebuildChunkBlockRenderInLayerEvent event) {
 
-		mods.betterfoliage.client.Hooks.canRenderBlockInLayer(event.getBlockState().getBlock(), event.getBlockState(), event.getBlockRenderLayer());
+		boolean shouldRender = mods.betterfoliage.client.Hooks.canRenderBlockInLayer(event.getBlockState().getBlock(), event.getBlockState(), event.getBlockRenderLayer());
 
-		event.setCanceled(true);
+		if (shouldRender) {
+			event.setResult(Event.Result.ALLOW);
+		} else {
+			event.setResult(Event.Result.DENY);
+		}
+
 	}
 
 }
