a10d137a942764de888037867fc8f2f85ce1197d
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-01-15 15:31:15 +1100

    Revert "disable OF hooks for testing"
    This reverts commit 2a68cc25fbe62e7098a6186bebad1936ac5ba11b.

--- src/main/java/io/github/cadiboo/renderchunkrebuildchunkhooks/event/optifine/RebuildChunkPostOptifineEvent.java
+++ src/main/java/io/github/cadiboo/renderchunkrebuildchunkhooks/event/optifine/RebuildChunkPostOptifineEvent.java
@@ -1,86 +1,86 @@
-//package io.github.cadiboo.renderchunkrebuildchunkhooks.event.optifine;
-//
-//import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPostEvent;
-//import io.github.cadiboo.renderchunkrebuildchunkhooks.mod.EnumEventType;
-//import net.minecraft.client.renderer.BufferBuilder;
-//import net.minecraft.client.renderer.RenderGlobal;
-//import net.minecraft.client.renderer.chunk.ChunkCompileTaskGenerator;
-//import net.minecraft.client.renderer.chunk.CompiledChunk;
-//import net.minecraft.client.renderer.chunk.RenderChunk;
-//import net.minecraft.client.renderer.chunk.VisGraph;
-//import net.minecraft.tileentity.TileEntity;
-//import net.minecraft.util.math.BlockPos;
-//import net.minecraft.world.IBlockAccess;
-//import net.minecraftforge.common.MinecraftForge;
-//import net.optifine.override.ChunkCacheOF;
-//
-//import javax.annotation.Nonnull;
-//import java.util.HashSet;
-//
-//import static io.github.cadiboo.renderchunkrebuildchunkhooks.hooks.RenderChunkRebuildChunkHooksHooksOptifine.getChunkCacheFromChunkCacheOF;
-//
-///**
-// * Called when a {@link RenderChunk#rebuildChunk} is called when Optifine is present.
-// * This event is fired on the {@link MinecraftForge#EVENT_BUS} right before RenderChunk#rebuildChunk returns.
-// * This event has access to all the filled {@link BufferBuilder}s and could allow you to export or modify that data
-// *
-// * @author Cadiboo
-// * @see RenderChunk#rebuildChunk(float, float, float, ChunkCompileTaskGenerator)
-// */
-//public class RebuildChunkPostOptifineEvent extends RebuildChunkPostEvent {
-//
-//	private final ChunkCacheOF chunkCacheOF;
-//
-//	/**
-//	 * @param renderChunk                     the instance of {@link RenderChunk}
-//	 * @param x                               the translation X passed in from RenderChunk#rebuildChunk
-//	 * @param y                               the translation Y passed in from RenderChunk#rebuildChunk
-//	 * @param z                               the translation Z passed in from RenderChunk#rebuildChunk
-//	 * @param generator                       the {@link ChunkCompileTaskGenerator} passed in from RenderChunk#rebuildChunk
-//	 * @param compiledChunk                   the {@link CompiledChunk} passed in from RenderChunk#rebuildChunk
-//	 * @param renderChunkPosition             the {@link BlockPos position} passed in from RenderChunk#rebuildChunk
-//	 * @param chunkCacheOF                    the {@link ChunkCacheOF} passed in from RenderChunk#rebuildChunk
-//	 * @param visGraph                        the {@link VisGraph} passed in from RenderChunk#rebuildChunk
-//	 * @param tileEntitiesWithGlobalRenderers the {@link HashSet} of {@link TileEntity TileEntities} with global renderers passed in from RenderChunk#rebuildChunk
-//	 * @param renderGlobal                    the {@link RenderGlobal} passed in from RenderChunk#rebuildChunk
-//	 */
-//	public RebuildChunkPostOptifineEvent(
-//			@Nonnull final RenderChunk renderChunk,
-//			final float x,
-//			final float y,
-//			final float z,
-//			@Nonnull final ChunkCompileTaskGenerator generator,
-//			@Nonnull final CompiledChunk compiledChunk,
-//			@Nonnull final BlockPos renderChunkPosition,
-//			@Nonnull final ChunkCacheOF chunkCacheOF,
-//			@Nonnull final VisGraph visGraph,
-//			@Nonnull final HashSet<TileEntity> tileEntitiesWithGlobalRenderers,
-//			@Nonnull final RenderGlobal renderGlobal) {
-//		super(renderChunk, x, y, z, generator, compiledChunk, renderChunkPosition, getChunkCacheFromChunkCacheOF(chunkCacheOF), visGraph, tileEntitiesWithGlobalRenderers, renderGlobal);
-//		this.chunkCacheOF = chunkCacheOF;
-//	}
-//
-//	/**
-//	 * @return the type of event
-//	 */
-//	@Nonnull
-//	@Override
-//	public EnumEventType getType() {
-//		return EnumEventType.FORGE_OPTIFINE;
-//	}
-//
-//	@Nonnull
-//	public ChunkCacheOF getChunkCacheOF() {
-//		return chunkCacheOF;
-//	}
-//
-//	/**
-//	 * @return the {@link ChunkCacheOF} passed in
-//	 */
-//	@Nonnull
-//	@Override
-//	public IBlockAccess getIBlockAccess() {
-//		return getChunkCacheOF();
-//	}
-//
-//}
+package io.github.cadiboo.renderchunkrebuildchunkhooks.event.optifine;
+
+import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPostEvent;
+import io.github.cadiboo.renderchunkrebuildchunkhooks.mod.EnumEventType;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.chunk.ChunkCompileTaskGenerator;
+import net.minecraft.client.renderer.chunk.CompiledChunk;
+import net.minecraft.client.renderer.chunk.RenderChunk;
+import net.minecraft.client.renderer.chunk.VisGraph;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.MinecraftForge;
+import net.optifine.override.ChunkCacheOF;
+
+import javax.annotation.Nonnull;
+import java.util.HashSet;
+
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.hooks.RenderChunkRebuildChunkHooksHooksOptifine.getChunkCacheFromChunkCacheOF;
+
+/**
+ * Called when a {@link RenderChunk#rebuildChunk} is called when Optifine is present.
+ * This event is fired on the {@link MinecraftForge#EVENT_BUS} right before RenderChunk#rebuildChunk returns.
+ * This event has access to all the filled {@link BufferBuilder}s and could allow you to export or modify that data
+ *
+ * @author Cadiboo
+ * @see RenderChunk#rebuildChunk(float, float, float, ChunkCompileTaskGenerator)
+ */
+public class RebuildChunkPostOptifineEvent extends RebuildChunkPostEvent {
+
+	private final ChunkCacheOF chunkCacheOF;
+
+	/**
+	 * @param renderChunk                     the instance of {@link RenderChunk}
+	 * @param x                               the translation X passed in from RenderChunk#rebuildChunk
+	 * @param y                               the translation Y passed in from RenderChunk#rebuildChunk
+	 * @param z                               the translation Z passed in from RenderChunk#rebuildChunk
+	 * @param generator                       the {@link ChunkCompileTaskGenerator} passed in from RenderChunk#rebuildChunk
+	 * @param compiledChunk                   the {@link CompiledChunk} passed in from RenderChunk#rebuildChunk
+	 * @param renderChunkPosition             the {@link BlockPos position} passed in from RenderChunk#rebuildChunk
+	 * @param chunkCacheOF                    the {@link ChunkCacheOF} passed in from RenderChunk#rebuildChunk
+	 * @param visGraph                        the {@link VisGraph} passed in from RenderChunk#rebuildChunk
+	 * @param tileEntitiesWithGlobalRenderers the {@link HashSet} of {@link TileEntity TileEntities} with global renderers passed in from RenderChunk#rebuildChunk
+	 * @param renderGlobal                    the {@link RenderGlobal} passed in from RenderChunk#rebuildChunk
+	 */
+	public RebuildChunkPostOptifineEvent(
+			@Nonnull final RenderChunk renderChunk,
+			final float x,
+			final float y,
+			final float z,
+			@Nonnull final ChunkCompileTaskGenerator generator,
+			@Nonnull final CompiledChunk compiledChunk,
+			@Nonnull final BlockPos renderChunkPosition,
+			@Nonnull final ChunkCacheOF chunkCacheOF,
+			@Nonnull final VisGraph visGraph,
+			@Nonnull final HashSet<TileEntity> tileEntitiesWithGlobalRenderers,
+			@Nonnull final RenderGlobal renderGlobal) {
+		super(renderChunk, x, y, z, generator, compiledChunk, renderChunkPosition, getChunkCacheFromChunkCacheOF(chunkCacheOF), visGraph, tileEntitiesWithGlobalRenderers, renderGlobal);
+		this.chunkCacheOF = chunkCacheOF;
+	}
+
+	/**
+	 * @return the type of event
+	 */
+	@Nonnull
+	@Override
+	public EnumEventType getType() {
+		return EnumEventType.FORGE_OPTIFINE;
+	}
+
+	@Nonnull
+	public ChunkCacheOF getChunkCacheOF() {
+		return chunkCacheOF;
+	}
+
+	/**
+	 * @return the {@link ChunkCacheOF} passed in
+	 */
+	@Nonnull
+	@Override
+	public IBlockAccess getIBlockAccess() {
+		return getChunkCacheOF();
+	}
+
+}
