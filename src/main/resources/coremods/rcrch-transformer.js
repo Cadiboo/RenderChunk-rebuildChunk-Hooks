2970c8d2f926be4873df7c444d3649cbed343dea
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-17 10:51:07 +1100

    Update rcrch-transformer.js

--- src/main/resources/coremods/rcrch-transformer.js
+++ src/main/resources/coremods/rcrch-transformer.js
@@ -19,9 +19,10 @@ function initializeCoreMod() {
 				for (var i = 0; i < arrayLength; i++) {
 					var method = methods[i];
 
-                    if(methodMatches(method)) {
-                        injectHooks(method.instructions);
-                    }
+					if(methodMatches(method)) {
+						print("Calling injectHooks");
+						injectHooks(method.instructions);
+					}
 
 				}
 
@@ -34,55 +35,58 @@ function initializeCoreMod() {
 var DEBUG_METHODS = true;
 
 var RENDER_CHUNK_REBUILD_CHUNK = {
-    'getName': function() {
-        "rebuildChunk"; //TODO obf func_178581_b
-    },
-    'getDescriptor': function() {
-        return "(FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V";
-    }
+	'getName': function() {
+		"rebuildChunk"; //TODO obf func_178581_b
+	},
+	'getDescriptor': function() {
+		return "(FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V";
+	}
 }
 
 var RENDER_CHUNK_RESORT_TRANSPARENCY = {
-    'getName': function() {
-        "resortTransparency"; //TODO obf func_178570_a
-    },
-    'getDescriptor': function() {
-        return "(FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V"
-    }
+	'getName': function() {
+		"resortTransparency"; //TODO obf func_178570_a
+	},
+	'getDescriptor': function() {
+		return "(FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V"
+	}
 }
 
 function methodMatches(method) {
-    if (!method.desc.equals(RENDER_CHUNK_REBUILD_CHUNK.getDescriptor())) {
-        if (DEBUG_METHODS) {
-            print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" did not match");
-        }
-        return false;
-    }
+	if (!method.desc.equals(RENDER_CHUNK_REBUILD_CHUNK.getDescriptor())) {
+		if (DEBUG_METHODS) {
+			print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" did not match");
+		}
+		return false;
+	}
 
-    if (DEBUG_METHODS) {
-        print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" matched!");
-    }
+	if (DEBUG_METHODS) {
+		print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" matched!");
+	}
 
-    // make sure not to overwrite resortTransparency (it has the same description but it's name is "a" while rebuildChunk's name is "b")
-    if (method.name.equals(RENDER_CHUNK_RESORT_TRANSPARENCY.getName())) {
-        if (DEBUG_METHODS) {
-            print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" was rejected");
-        }
-        return false;
-    }
+	// make sure not to overwrite resortTransparency (it has the same description but it's name is "a" while rebuildChunk's name is "b")
+	if (method.name.equals(RENDER_CHUNK_RESORT_TRANSPARENCY.getName())) {
+		if (DEBUG_METHODS) {
+			print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" was rejected");
+		}
+		return false;
+	}
 
-    if (DEBUG_METHODS) {
-        print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" matched and passed");
-    }
+	if (DEBUG_METHODS) {
+		print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" matched and passed");
+	}
 
-    return true;
+	return true;
 }
 
 function injectHooks(instructions) {
 
-    var instruction;
-    for (instruction in instructions) {
-        print(instruction);
-    }
+	print("Inside injectHooks");
+
+	var arrayLength = instructions.length;
+	for (var i = 0; i < arrayLength; i++) {
+		var instruction = instructions[i];
+		print(instruction);
+	}
 
 }
