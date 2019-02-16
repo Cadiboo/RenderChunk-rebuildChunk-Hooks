f336cbed21d79f11a142311b8035ca033c6e18c0
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-16 21:38:26 +1100

    Update rcrch-transformer.js

--- src/main/resources/coremods/rcrch-transformer.js
+++ src/main/resources/coremods/rcrch-transformer.js
@@ -7,14 +7,28 @@ function initializeCoreMod() {
             },
             'transformer': function(classNode) {
                 print("RenderChunk: ", classNode.name);
-//                var methodnode = Java.type('org.objectweb.asm.tree.MethodNode')
-//                var opcodes = Java.type('org.objectweb.asm.Opcodes')
-//                var tmp = new methodnode(opcodes.ASM5);
-//                tmp.name = 'dummyMethod';
-//                tmp.visitVarInsn(opcodes.ALOAD, 0);
-//                tmp.visitMethodInsn(opcodes.INVOKESTATIC, "net/minecraftforge/fml/FMLTransformers", "hackName", "()Ljava/lang/String;", false);
-//                tmp.visitFieldInsn(opcodes.PUTFIELD, "net/minecraft/client/gui/GuiMainMenu", "field_73975_c", "Ljava/lang/String;");
-//                classNode.methods.add(tmp);
+                var/*Class*/ MethodNode = Java.type('org.objectweb.asm.tree.MethodNode')
+                var/*Interface*/ Opcodes = Java.type('org.objectweb.asm.Opcodes')
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+
+
+
                 return classNode;
             }
         }
