b6b2c4189f0e569ae0b54dc1930d40b09fc15af4
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-17 12:11:24 +1100

    TEMP!!!!

---
+++ src/main/java/org/objectweb/asm/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Repackage by Cadiboo. Can't put it in a repackage package because loader exclusions
+ * //BIG TODO: remove this, its just for some debugging. It can be transferred to JS
+ */
+
+package org.objectweb.asm.util;
