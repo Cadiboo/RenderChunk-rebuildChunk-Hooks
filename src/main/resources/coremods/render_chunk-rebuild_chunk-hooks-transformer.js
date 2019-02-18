d2a358750476ad759051ce7002d9dd5ac54e267c
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-18 17:43:25 +1100

    Update render_chunk-rebuild_chunk-hooks-transformer.js

--- src/main/resources/coremods/render_chunk-rebuild_chunk-hooks-transformer.js
+++ src/main/resources/coremods/render_chunk-rebuild_chunk-hooks-transformer.js
@@ -28,67 +28,16 @@ function initializeCoreMod() {
 	}
 }
 
-var/*Class/Interface*/ Opcodes = Java.type('org.objectweb.asm.Opcodes');
-var/*Class*/ MethodNode = Java.type('org.objectweb.asm.tree.MethodNode');
-var/*Class*/ MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
-var/*Class*/ InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
-var/*Class*/ VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
-var/*Class*/ InsnList = Java.type('org.objectweb.asm.tree.InsnList');
-
-var ALOAD = Opcodes.ALOAD;
-var FLOAD = Opcodes.FLOAD;
-var RETURN = Opcodes.RETURN;
-var INVOKESTATIC = Opcodes.INVOKESTATIC;
-
 var DEBUG_METHODS = true;
-var DEBUG_INSTRUCTIONS = false;
 
-var RENDER_CHUNK_REBUILD_CHUNK = {
-	'getName': function() {
-		//TODO (de)obf
-//		return "rebuildChunk";
-		return "func_178581_b";
-	},
-	'getDescriptor': function() {
-		return "(FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V";
-	}
+function methodNameMatches(methodName) {
+	return "rebuildChunk".equals(methodName) || "func_178581_b".equals(methodName)
 }
 
-var RENDER_CHUNK_RESORT_TRANSPARENCY = {
-	'getName': function() {
-		//TODO (de)obf
-//		return "resortTransparency";
-		return "func_178570_a";
-	},
-	'getDescriptor': function() {
-		return "(FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V"
-	}
-}
-
-var REBUILD_CHUNK_REDIRECT_TEMP =  {
-	'visit': function(insnList) {
-		// invokestatic io/github/cadiboo/renderchunkrebuildchunkhooks/HooksTemp(Lnet/minecraft/client/renderer/chunk/RenderChunk;FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V
-		insnList.add(
-			new MethodInsnNode(INVOKESTATIC,
-				"io/github/cadiboo/renderchunkrebuildchunkhooks/HooksTemp",
-				 "rebuildChunk",
-				 "(Lnet/minecraft/client/renderer/chunk/RenderChunk;FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V",
-				 false
-			)
-		);
-
-//		public MethodInsnNode(
-//              final int opcode,
-//              final String owner,
-//              final String name,
-//              final String descriptor,
-//              final boolean isInterface) {
-
-	}
-}
+var RENDER_CHUNK_REBUILD_CHUNK_DESCRIPTOR = "(FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V";
 
 function methodMatches(method) {
-	if (!method.desc.equals(RENDER_CHUNK_REBUILD_CHUNK.getDescriptor())) {
+	if (!method.desc.equals(RENDER_CHUNK_REBUILD_CHUNK_DESCRIPTOR)) {
 		if (DEBUG_METHODS) {
 			print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" did not match");
 		}
@@ -99,8 +48,8 @@ function methodMatches(method) {
 		print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" matched!");
 	}
 
-	// make sure not to overwrite resortTransparency (it has the same description but it's name is "a" while rebuildChunk's name is "b")
-	if (!method.name.equals(RENDER_CHUNK_REBUILD_CHUNK.getName())) {
+	// make sure not to overwrite resortTransparency (it has the same description but it's name is "a" or "func_178570_a" while rebuildChunk's name is "b" or "func_178581_b")
+	if (!methodNameMatches(method.name)) {
 		if (DEBUG_METHODS) {
 			print("Method with name \"" + method.name + "\" and description \"" + method.desc + "\" was rejected");
 		}
@@ -119,18 +68,28 @@ function injectHooks(instructions) {
 	print("Inside injectHooks");
 	print(instructions);
 
-	if (DEBUG_INSTRUCTIONS) {
-		var instructionsArray = instructions.toArray();
-		var arrayLength = instructionsArray.length;
-		for (var i = 0; i < arrayLength; i++) {
-			var instruction = instructionsArray[i];
-			print(insnToString(instruction));
-		}
-	}
+//	var instructionsArray = instructions.toArray();
+//	var arrayLength = instructionsArray.length;
+//	for (var i = 0; i < arrayLength; i++) {
+//		var instruction = instructionsArray[i];
+//		print(insnToString(instruction));
+//	}
 
 	overwriteMethodTemp(instructions);
 
 }
+
+var/*Class/Interface*/ Opcodes = Java.type('org.objectweb.asm.Opcodes');
+var/*Class*/ MethodNode = Java.type('org.objectweb.asm.tree.MethodNode');
+var/*Class*/ MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
+var/*Class*/ InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
+var/*Class*/ VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
+var/*Class*/ InsnList = Java.type('org.objectweb.asm.tree.InsnList');
+
+var ALOAD = Opcodes.ALOAD;
+var FLOAD = Opcodes.FLOAD;
+var RETURN = Opcodes.RETURN;
+var INVOKESTATIC = Opcodes.INVOKESTATIC;
 
 var ALOAD_this = 0;
 var FLOAD_x = 1;
@@ -176,84 +135,24 @@ function overwriteMethodTemp(instructions) {
 	print("INJECTED SUCCESSFULLY!!!");
 	print("INJECTED SUCCESSFULLY!!!");
 
-	// aload0
-	// float1
-	// float2
-	// float3
-	// aload4
-	// invokestatic io/github/cadiboo/renderchunkrebuildchunkhooks/HooksTemp(Lnet/minecraft/client/renderer/chunk/RenderChunk;FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V
-	//return
-
-	// io.github.cadiboo.renderchunkrebuildchunkhooks.HooksTemp
-
-	//public static void rebuildChunk(
-	//			final RenderChunk renderChunk,
-	//			float x, float y, float z, ChunkRenderTask generator
-	//	) {
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var splittableString = "NOP,ACONST_NULL,ICONST_M1,ICONST_0,ICONST_1,ICONST_2,"
-	+ "ICONST_3,ICONST_4,ICONST_5,LCONST_0,LCONST_1,FCONST_0,"
-	+ "FCONST_1,FCONST_2,DCONST_0,DCONST_1,BIPUSH,SIPUSH,LDC,,,"
-	+ "ILOAD,LLOAD,FLOAD,DLOAD,ALOAD,,,,,,,,,,,,,,,,,,,,,IALOAD,"
-	+ "LALOAD,FALOAD,DALOAD,AALOAD,BALOAD,CALOAD,SALOAD,ISTORE,"
-	+ "LSTORE,FSTORE,DSTORE,ASTORE,,,,,,,,,,,,,,,,,,,,,IASTORE,"
-	+ "LASTORE,FASTORE,DASTORE,AASTORE,BASTORE,CASTORE,SASTORE,POP,"
-	+ "POP2,DUP,DUP_X1,DUP_X2,DUP2,DUP2_X1,DUP2_X2,SWAP,IADD,LADD,"
-	+ "FADD,DADD,ISUB,LSUB,FSUB,DSUB,IMUL,LMUL,FMUL,DMUL,IDIV,LDIV,"
-	+ "FDIV,DDIV,IREM,LREM,FREM,DREM,INEG,LNEG,FNEG,DNEG,ISHL,LSHL,"
-	+ "ISHR,LSHR,IUSHR,LUSHR,IAND,LAND,IOR,LOR,IXOR,LXOR,IINC,I2L,"
-	+ "I2F,I2D,L2I,L2F,L2D,F2I,F2L,F2D,D2I,D2L,D2F,I2B,I2C,I2S,LCMP,"
-	+ "FCMPL,FCMPG,DCMPL,DCMPG,IFEQ,IFNE,IFLT,IFGE,IFGT,IFLE,"
-	+ "IF_ICMPEQ,IF_ICMPNE,IF_ICMPLT,IF_ICMPGE,IF_ICMPGT,IF_ICMPLE,"
-	+ "IF_ACMPEQ,IF_ACMPNE,GOTO,JSR,RET,TABLESWITCH,LOOKUPSWITCH,"
-	+ "IRETURN,LRETURN,FRETURN,DRETURN,ARETURN,RETURN,GETSTATIC,"
-	+ "PUTSTATIC,GETFIELD,PUTFIELD,INVOKEVIRTUAL,INVOKESPECIAL,"
-	+ "INVOKESTATIC,INVOKEINTERFACE,INVOKEDYNAMIC,NEW,NEWARRAY,"
-	+ "ANEWARRAY,ARRAYLENGTH,ATHROW,CHECKCAST,INSTANCEOF,"
-	+ "MONITORENTER,MONITOREXIT,,MULTIANEWARRAY,IFNULL,IFNONNULL,";
-var opcodesLookupArray = splittableString.split(",");
-//
-//splittableString = "T_BOOLEAN,T_CHAR,T_FLOAT,T_DOUBLE,T_BYTE,T_SHORT,T_INT,T_LONG,";
-//var typesLookupArray = splittableString.split(",");
-//
-//splittableString = "H_GETFIELD,H_GETSTATIC,H_PUTFIELD,H_PUTSTATIC,"
-//	+ "H_INVOKEVIRTUAL,H_INVOKESTATIC,H_INVOKESPECIAL,"
-//	+ "H_NEWINVOKESPECIAL,H_INVOKEINTERFACE,";
-//var handlesLookupArray = splittableString.split(",");
-
-function insnToString(insn) {
-	try {
-		return opcodesLookupArray[insn.opcode];
-	} catch (e) {
-		print(e);
+var REBUILD_CHUNK_REDIRECT_TEMP =  {
+	'visit': function(insnList) {
+		// invokestatic io/github/cadiboo/renderchunkrebuildchunkhooks/HooksTemp(Lnet/minecraft/client/renderer/chunk/RenderChunk;FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V
+		insnList.add(
+			new MethodInsnNode(
+				//int opcode
+				INVOKESTATIC,
+				//String owner
+				"io/github/cadiboo/renderchunkrebuildchunkhooks/HooksTemp",
+				//String name
+				"rebuildChunk",
+				//String descriptor
+				"(Lnet/minecraft/client/renderer/chunk/RenderChunk;FFFLnet/minecraft/client/renderer/chunk/ChunkRenderTask;)V",
+				//boolean isInterface
+				false
+			)
+		);
 	}
-	return "";
 }
