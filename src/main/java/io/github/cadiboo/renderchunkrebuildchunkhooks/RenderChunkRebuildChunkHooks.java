909a166e91d7c308939ae376d32cf8e459de1f43
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-02-19 16:59:42 +1100

    HookConfig n stuff

--- src/main/java/io/github/cadiboo/renderchunkrebuildchunkhooks/RenderChunkRebuildChunkHooks.java
+++ src/main/java/io/github/cadiboo/renderchunkrebuildchunkhooks/RenderChunkRebuildChunkHooks.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
  * @author Cadiboo
  */
 @Mod(Refs.MOD_ID)
-public class RenderChunkRebuildChunkHooks {
+public final class RenderChunkRebuildChunkHooks {
 
 	private static final Logger LOGGER = LogManager.getLogger(Refs.MOD_ID);
 
@@ -21,6 +21,115 @@ public class RenderChunkRebuildChunkHooks {
 		LOGGER.info("Initialising RenderChunk");
 		final int unused = RenderChunk.renderChunksUpdated;
 		LOGGER.info("Successfully initialised RenderChunk");
+	}
+
+	/**
+	 * Reduces overhead by not creating or posting events if no mods require them
+	 */
+	public static class HookConfig {
+
+		//	RebuildChunkPreEvent
+		//	RebuildChunkPreRenderSetupEvent
+		//	RebuildChunkPreRenderEvent
+		//	RebuildChunkCanFluidRenderInLayerEvent
+		//	RebuildChunkFluidEvent
+		//	RebuildChunkCanBlockRenderWithTypeEvent
+		//	RebuildChunkCanBlockRenderInLayerEvent
+		//	RebuildChunkBlockEvent
+		//	RebuildChunkPostRenderEvent
+		//	RebuildChunkPostEvent
+
+		private static boolean postRebuildChunkPreEvent = false;
+		private static boolean postRebuildChunkPreRenderSetupEvent = false;
+		private static boolean postRebuildChunkPreRenderEvent = false;
+		private static boolean postRebuildChunkCanFluidRenderInLayerEvent = false;
+		private static boolean postRebuildChunkFluidEvent = false;
+		private static boolean postRebuildChunkCanBlockRenderWithTypeEvent = false;
+		private static boolean postRebuildChunkCanBlockRenderInLayerEvent = false;
+		private static boolean postRebuildChunkBlockEvent = false;
+		private static boolean postRebuildChunkPostEvent = false;
+		private static boolean postRebuildChunkPostRenderEvent = false;
+
+		public static void enableRebuildChunkPreEvent() {
+			postRebuildChunkPreEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkPreEvent() {
+			return postRebuildChunkPreEvent;
+		}
+
+		public static void enableRebuildChunkPreRenderSetupEvent() {
+			postRebuildChunkPreRenderSetupEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkPreRenderSetupEvent() {
+			return postRebuildChunkPreRenderSetupEvent;
+		}
+
+		public static void enableRebuildChunkPreRenderEvent() {
+			postRebuildChunkPreRenderEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkPreRenderEvent() {
+			return postRebuildChunkPreRenderEvent;
+		}
+
+		public static void enableRebuildChunkCanFluidRenderInLayerEvent() {
+			postRebuildChunkCanFluidRenderInLayerEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkCanFluidRenderInLayerEvent() {
+			return postRebuildChunkCanFluidRenderInLayerEvent;
+		}
+
+		public static void enableRebuildChunkFluidEvent() {
+			postRebuildChunkFluidEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkFluidEvent() {
+			return postRebuildChunkFluidEvent;
+		}
+
+		public static void enableRebuildChunkCanBlockRenderWithTypeEvent() {
+			postRebuildChunkCanBlockRenderWithTypeEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkCanBlockRenderWithTypeEvent() {
+			return postRebuildChunkCanBlockRenderWithTypeEvent;
+		}
+
+		public static void enableRebuildChunkCanBlockRenderInLayerEvent() {
+			postRebuildChunkCanBlockRenderInLayerEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkCanBlockRenderInLayerEvent() {
+			return postRebuildChunkCanBlockRenderInLayerEvent;
+		}
+
+		public static void enableRebuildChunkBlockEvent() {
+			postRebuildChunkBlockEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkBlockEvent() {
+			return postRebuildChunkBlockEvent;
+		}
+
+		public static void enableRebuildChunkPostRenderEvent() {
+			postRebuildChunkPostRenderEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkPostRenderEvent() {
+			return postRebuildChunkPostRenderEvent;
+		}
+
+		public static void enableRebuildChunkPostEvent() {
+			postRebuildChunkPostEvent = true;
+		}
+
+		public static boolean shouldPostRebuildChunkPostEvent() {
+			return postRebuildChunkPostEvent;
+		}
+
 	}
 
 }
--- src/main/java/io/github/cadiboo/renderchunkrebuildchunkhooks/hooks/RenderChunkRebuildChunkHooksHooks.java
+++ src/main/java/io/github/cadiboo/renderchunkrebuildchunkhooks/hooks/RenderChunkRebuildChunkHooksHooks.java
@@ -4,12 +4,12 @@ import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkBlockEve
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkCanBlockRenderInLayerEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkCanBlockRenderWithTypeEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkCanFluidRenderInLayerEvent;
-import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkFluidBeRenderedEvent;
+import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkFluidEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPostEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPostRenderEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPreEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPreRenderEvent;
-import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPreRenderingSetupEvent;
+import io.github.cadiboo.renderchunkrebuildchunkhooks.event.RebuildChunkPreRenderSetupEvent;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.util.RenderChunkCacheReference;
 import io.github.cadiboo.renderchunkrebuildchunkhooks.util.WorldReference;
 import net.minecraft.block.Block;
@@ -31,6 +31,17 @@ import net.minecraftforge.common.MinecraftForge;
 import java.util.HashSet;
 import java.util.Random;
 
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkBlockEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkCanBlockRenderInLayerEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkCanBlockRenderWithTypeEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkCanFluidRenderInLayerEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkFluidEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkPostEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkPostRenderEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkPreEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkPreRenderEvent;
+import static io.github.cadiboo.renderchunkrebuildchunkhooks.RenderChunkRebuildChunkHooks.HookConfig.shouldPostRebuildChunkPreRenderSetupEvent;
+
 /**
  * @author Cadiboo
  */
@@ -38,6 +49,9 @@ public final class RenderChunkRebuildChunkHooksHooks {
 
 	//return if rendering should _not_ happen
 	public static boolean rebuildChunkCancelRenderingPreGeneratingCompiledChunkHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final WorldReference worldReference) {
+		if (!shouldPostRebuildChunkPreEvent()) {
+			return false;
+		}
 		final RebuildChunkPreEvent event = new RebuildChunkPreEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, worldReference);
 		MinecraftForge.EVENT_BUS.post(event);
 		return event.isCanceled();
@@ -45,18 +59,28 @@ public final class RenderChunkRebuildChunkHooksHooks {
 
 	//return if rendering should _not_ happen
 	public static boolean rebuildChunkCancelRenderingPreRenderSetupHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCacheReference lvt_10_1_Reference, final VisGraph lvt_11_1_, final HashSet lvt_12_1_) {
-		final RebuildChunkPreRenderingSetupEvent event = new RebuildChunkPreRenderingSetupEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_Reference, lvt_11_1_, lvt_12_1_);
+		if (!shouldPostRebuildChunkPreRenderSetupEvent()) {
+			return false;
+		}
+		final RebuildChunkPreRenderSetupEvent event = new RebuildChunkPreRenderSetupEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_Reference, lvt_11_1_, lvt_12_1_);
 		MinecraftForge.EVENT_BUS.post(event);
 		return event.isCanceled();
 	}
 
 	public static void rebuildChunkPreRenderHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher) {
+		if (!shouldPostRebuildChunkPreRenderEvent()) {
+			return;
+		}
 		final RebuildChunkPreRenderEvent event = new RebuildChunkPreRenderEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_, lvt_11_1_, lvt_12_1_, aboolean, random, blockrendererdispatcher);
 		MinecraftForge.EVENT_BUS.post(event);
 	}
 
 	//return if the fluid _can_ render in the layer
 	public static boolean rebuildChunkCanFluidRenderInLayerHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final BlockPos blockpos$mutableblockpos, final IFluidState ifluidstate, final BlockRenderLayer blockrenderlayer1) {
+		if (!shouldPostRebuildChunkCanFluidRenderInLayerEvent()) {
+			//TODO: keep in sync with un-hooked behaviour
+			return ifluidstate.canRenderInLayer(blockrenderlayer1);
+		}
 		final RebuildChunkCanFluidRenderInLayerEvent event = new RebuildChunkCanFluidRenderInLayerEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_, lvt_11_1_, lvt_12_1_, aboolean, random, blockrendererdispatcher, blockpos$mutableblockpos, ifluidstate, blockrenderlayer1);
 		MinecraftForge.EVENT_BUS.post(event);
 		switch (event.getResult()) {
@@ -73,7 +97,10 @@ public final class RenderChunkRebuildChunkHooksHooks {
 
 	//return if the fluid _should be rendered by vanilla_
 	public static boolean rebuildChunkCanFluidBeRenderedHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final BlockPos blockpos$mutableblockpos, final IFluidState ifluidstate, final BlockRenderLayer blockrenderlayer1, final int j, final BufferBuilder bufferbuilder) {
-		final RebuildChunkFluidBeRenderedEvent event = new RebuildChunkFluidBeRenderedEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_, lvt_11_1_, lvt_12_1_, aboolean, random, blockrendererdispatcher, blockpos$mutableblockpos, ifluidstate, blockrenderlayer1, j, bufferbuilder);
+		if (!shouldPostRebuildChunkFluidEvent()) {
+			return true;
+		}
+		final RebuildChunkFluidEvent event = new RebuildChunkFluidEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_, lvt_11_1_, lvt_12_1_, aboolean, random, blockrendererdispatcher, blockpos$mutableblockpos, ifluidstate, blockrenderlayer1, j, bufferbuilder);
 		MinecraftForge.EVENT_BUS.post(event);
 		return !event.isCanceled();
 	}
@@ -81,7 +108,10 @@ public final class RenderChunkRebuildChunkHooksHooks {
 	//return if the block _can_ render in the type (iblockstate.getRenderType()) and layer
 	public static boolean rebuildChunkCanBlockRenderWithTypeAndInLayerHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final BlockPos blockpos$mutableblockpos, final IBlockState iblockstate, final Block block, final BlockRenderLayer blockrenderlayer1) {
 		boolean canRenderInType = false;
-		{
+		if (!shouldPostRebuildChunkCanBlockRenderWithTypeEvent()) {
+			//TODO: keep in sync with un-hooked behaviour
+			canRenderInType = iblockstate.getRenderType() != EnumBlockRenderType.INVISIBLE;
+		} else {
 			final EnumBlockRenderType renderType = iblockstate.getRenderType();
 			final RebuildChunkCanBlockRenderWithTypeEvent event = new RebuildChunkCanBlockRenderWithTypeEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_, lvt_11_1_, lvt_12_1_, aboolean, random, blockrendererdispatcher, blockpos$mutableblockpos, iblockstate, block, renderType);
 			MinecraftForge.EVENT_BUS.post(event);
@@ -100,7 +130,10 @@ public final class RenderChunkRebuildChunkHooksHooks {
 		}
 
 		boolean canRenderInLayer = false;
-		{
+		if (!shouldPostRebuildChunkCanBlockRenderInLayerEvent()) {
+			//TODO: keep in sync with un-hooked behaviour
+			canRenderInLayer = iblockstate.canRenderInLayer(blockrenderlayer1);
+		} else {
 			final RebuildChunkCanBlockRenderInLayerEvent event = new RebuildChunkCanBlockRenderInLayerEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_, lvt_11_1_, lvt_12_1_, aboolean, random, blockrendererdispatcher, blockpos$mutableblockpos, iblockstate, block, blockrenderlayer1);
 			MinecraftForge.EVENT_BUS.post(event);
 			switch (event.getResult()) {
@@ -122,17 +155,26 @@ public final class RenderChunkRebuildChunkHooksHooks {
 
 	//return if the block _should be rendered by vanilla_
 	public static boolean rebuildChunkCanBlockBeRenderedHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final BlockPos blockpos$mutableblockpos, final IBlockState iblockstate, final Block block, final BlockRenderLayer blockrenderlayer1, final int k, final BufferBuilder bufferbuilder1) {
+		if (!shouldPostRebuildChunkBlockEvent()) {
+			return true;
+		}
 		final RebuildChunkBlockEvent event = new RebuildChunkBlockEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_, lvt_11_1_, lvt_12_1_, aboolean, random, blockrendererdispatcher, blockpos$mutableblockpos, iblockstate, block, blockrenderlayer1, k, bufferbuilder1);
 		MinecraftForge.EVENT_BUS.post(event);
 		return !event.isCanceled();
 	}
 
 	public static void rebuildChunkPostRenderHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher) {
+		if (!shouldPostRebuildChunkPostRenderEvent()) {
+			return;
+		}
 		final RebuildChunkPostRenderEvent event = new RebuildChunkPostRenderEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world, lvt_10_1_, lvt_11_1_, lvt_12_1_, aboolean, random, blockrendererdispatcher);
 		MinecraftForge.EVENT_BUS.post(event);
 	}
 
 	public static void rebuildChunkPostHook(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world) {
+		if (!shouldPostRebuildChunkPostEvent()) {
+			return;
+		}
 		final RebuildChunkPostEvent event = new RebuildChunkPostEvent(renderChunk, x, y, z, generator, compiledchunk, blockpos, blockpos1, world);
 		MinecraftForge.EVENT_BUS.post(event);
 	}
