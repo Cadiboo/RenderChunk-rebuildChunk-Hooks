6bba4e1a00f5d47bcca2a6ac72545093a0baf98b
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-04-08 23:20:08 +1000

    yay

--- src/main/java/io/github/cadiboo/renderchunkrebuildchunkhooks/event/PooledHandler.java
+++ src/main/java/io/github/cadiboo/renderchunkrebuildchunkhooks/event/PooledHandler.java
@@ -10,11 +10,15 @@ import net.minecraft.client.renderer.chunk.CompiledChunk;
 import net.minecraft.client.renderer.chunk.RenderChunk;
 import net.minecraft.client.renderer.chunk.RenderChunkCache;
 import net.minecraft.client.renderer.chunk.VisGraph;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.ReportedException;
 import net.minecraft.fluid.IFluidState;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.eventbus.api.Event;
 
+import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Random;
 
@@ -27,15 +31,44 @@ import java.util.Random;
 @Deprecated
 public final class PooledHandler {
 
+	private static final Field Event_phase;
 	private static final ThreadLocal<RebuildChunkIsFluidEmptyEvent> threadRebuildChunkIsFluidEmptyEvent = ThreadLocal.withInitial(() -> new RebuildChunkIsFluidEmptyEvent(false));
 	private static final ThreadLocal<RebuildChunkCanFluidRenderInLayerEvent> threadRebuildChunkCanFluidRenderInLayerEvent = ThreadLocal.withInitial(() -> new RebuildChunkCanFluidRenderInLayerEvent(false));
 	private static final ThreadLocal<RebuildChunkRenderFluidEvent> threadRebuildChunkRenderFluidEvent = ThreadLocal.withInitial(() -> new RebuildChunkRenderFluidEvent(false));
 	private static final ThreadLocal<RebuildChunkCanBlockRenderTypeBeRenderedEvent> threadRebuildChunkCanBlockRenderTypeBeRenderedEvent = ThreadLocal.withInitial(() -> new RebuildChunkCanBlockRenderTypeBeRenderedEvent(false));
 	private static final ThreadLocal<RebuildChunkCanBlockRenderInLayerEvent> threadRebuildChunkCanBlockRenderInLayerEvent = ThreadLocal.withInitial(() -> new RebuildChunkCanBlockRenderInLayerEvent(false));
 	private static final ThreadLocal<RebuildChunkRenderBlockEvent> threadRebuildChunkRenderBlockEvent = ThreadLocal.withInitial(() -> new RebuildChunkRenderBlockEvent(false));
+	static {
+		try {
+			Field f = Event.class.getDeclaredField("phase");
+			f.setAccessible(true);
+			Event_phase = f;
+		} catch (Exception e) {
+			throw new ReportedException(
+					CrashReport.makeCrashReport(e, "Failed to find field Event.phase")
+			);
+		}
+	}
+
+	private static void reset(final Event event) {
+		try {
+			Event_phase.set(event, null);
+		} catch (IllegalAccessException e) {
+			throw new ReportedException(
+					CrashReport.makeCrashReport(e, "Failed to set field Event.phase")
+			);
+		}
+		if (event.isCancelable()) {
+			event.setCanceled(false);
+		}
+		if (event.hasResult()) {
+			event.setResult(Event.Result.DEFAULT);
+		}
+	}
 
 	public static RebuildChunkIsFluidEmptyEvent setupRebuildChunkIsFluidEmptyEvent(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final IBlockState iblockstate, final Block block, final IFluidState ifluidstate) {
 		final RebuildChunkIsFluidEmptyEvent event = threadRebuildChunkIsFluidEmptyEvent.get();
+		reset(event);
 		event.renderChunk = renderChunk;
 		event.x = x;
 		event.y = y;
@@ -77,6 +110,7 @@ public final class PooledHandler {
 
 	public static RebuildChunkCanFluidRenderInLayerEvent setupRebuildChunkCanFluidRenderInLayerEvent(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final IBlockState iblockstate, final Block block, final IFluidState ifluidstate, final BlockRenderLayer blockrenderlayer1) {
 		final RebuildChunkCanFluidRenderInLayerEvent event = threadRebuildChunkCanFluidRenderInLayerEvent.get();
+		reset(event);
 		event.renderChunk = renderChunk;
 		event.x = x;
 		event.y = y;
@@ -120,6 +154,7 @@ public final class PooledHandler {
 
 	public static RebuildChunkRenderFluidEvent setupRebuildChunkRenderFluidEvent(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final IBlockState iblockstate, final Block block, final IFluidState ifluidstate, final BlockRenderLayer blockrenderlayer1, final int j, final BufferBuilder bufferbuilder) {
 		final RebuildChunkRenderFluidEvent event = threadRebuildChunkRenderFluidEvent.get();
+		reset(event);
 		event.renderChunk = renderChunk;
 		event.x = x;
 		event.y = y;
@@ -166,6 +201,7 @@ public final class PooledHandler {
 
 	public static RebuildChunkCanBlockRenderTypeBeRenderedEvent setupRebuildChunkCanBlockRenderTypeBeRenderedEvent(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final IBlockState iblockstate, final Block block, final IFluidState ifluidstate) {
 		final RebuildChunkCanBlockRenderTypeBeRenderedEvent event = threadRebuildChunkCanBlockRenderTypeBeRenderedEvent.get();
+		reset(event);
 		event.renderChunk = renderChunk;
 		event.x = x;
 		event.y = y;
@@ -207,6 +243,7 @@ public final class PooledHandler {
 
 	public static RebuildChunkCanBlockRenderInLayerEvent setupRebuildChunkCanBlockRenderInLayerEvent(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final IBlockState iblockstate, final Block block, final IFluidState ifluidstate, final BlockRenderLayer blockrenderlayer1) {
 		final RebuildChunkCanBlockRenderInLayerEvent event = threadRebuildChunkCanBlockRenderInLayerEvent.get();
+		reset(event);
 		event.renderChunk = renderChunk;
 		event.x = x;
 		event.y = y;
@@ -250,6 +287,7 @@ public final class PooledHandler {
 
 	public static RebuildChunkRenderBlockEvent setupRebuildChunkRenderBlockEvent(final RenderChunk renderChunk, final float x, final float y, final float z, final ChunkRenderTask generator, final CompiledChunk compiledchunk, final BlockPos blockpos, final BlockPos blockpos1, final World world, final RenderChunkCache lvt_10_1_, final VisGraph lvt_11_1_, final HashSet lvt_12_1_, final boolean[] aboolean, final Random random, final BlockRendererDispatcher blockrendererdispatcher, final IBlockState iblockstate, final Block block, final IFluidState ifluidstate, final BlockRenderLayer blockrenderlayer1, final int k, final BufferBuilder bufferbuilder1) {
 		final RebuildChunkRenderBlockEvent event = threadRebuildChunkRenderBlockEvent.get();
+		reset(event);
 		event.renderChunk = renderChunk;
 		event.x = x;
 		event.y = y;
